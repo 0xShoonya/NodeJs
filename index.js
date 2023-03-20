1633ba536aaa19d3db3040c7ed972423117801c7
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-03-20 22:59:47 +0530

    Created Http server

---
+++ index.js
@@ -0,0 +1,20 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/about"){
+        res.end("<h1>About Page</h1>")
+    }
+    else if(req.url === "/contact"){
+        res.end("<h1>Contact Page</h1>")
+    }
+     else if(req.url === "/"){
+        res.end("<h1>Home Page</h1>")
+    }
+    else {
+        res.end("<h1>Page not found</h1>")
+    }
+})
+
+server.listen(5000, () => {
+    console.log("Server is running");
+})
